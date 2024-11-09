1b86a5723822be874565aee66c764217cc56b5bb
krilinxito <barryallen4207@gmail.com>
2024-11-08 21:26:45 -0400

    Primeros botones

--- projects/00-Hola-Mundo/src/main.jsx
+++ projects/00-Hola-Mundo/src/main.jsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from 'react';
+import { createRoot } from 'react-dom/client';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const root = createRoot(document.getElementById('root'));
+
+const Button = ({ Text }) => {
+  return (
+    <button>{Text}</button>
+  );
+};
+
+root.render(
+  <React.Fragment>
+    <Button Text={"Hola Mundo"} />
+    <Button Text={"Adios Mundo"} />
+  </React.Fragment>
+);
